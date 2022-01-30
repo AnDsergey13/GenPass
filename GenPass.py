c03f638169ccc0093fd1b3abaf27a2cfd7da40e7
Anakin <44977098+AnD13@users.noreply.github.com>
2022-01-31 01:55:38 +0300

    feature(all: gen_pass, main, gitignore): rewrite all application
    A lot of including:
    - add work with console (input, messages) (not finish!)
    - split gen_pass into gen_pass and main
    - rewrite documentation inside comments
    - change logic
    
    [adaptation to the console version]

--- GenPass.py
+++ GenPass.py
@@ -1,24 +1,18 @@
-# Генератор паролей
-# Приватный ключ(ПК)(уникальное слово, которое я всегда буду помнить)
-# Фраза ориентир(ФО)(полу публичный ключ)(фраза которая будет связана с местом, где нужно пароль вводить)
-# Локальный уникальный ключ(ЛУК)(файл со случайно сгенерированными символами)(привязка программы к конкретному устройству)(Если повредить этот файл, то даже владея приватным ключом и фразой ориентиром, подобрать пароль будет невозможно)
-# 
 """ 
 1) Получаем хэш ФО
 2) Полечаем хэш ПК
 3) полученные результаты из п.1 и п.2 - ксорятся(xor)
 4) Если не создан ЛУК, то
 	4.1) создаём файл ЛУК со случайными символами в нем и длиной L(длина от 1000 символов)
-	4.2) генерируем случайную строку также длиной L
-	4.3) Ксорим внутренности файла и строку, результат записываем в ЛУК-файл
-	- 4.4) повторяем п.4.3 от 5 до 10 раз(для увеличениии энтропии ЛУК)
-	4.5) Сохраняем изменения в ЛУК файл
-5) Получаем Хэш файла 
+5) Получаем Хэш ЛУК файла 
 6) ксорим его с результатом п.3
 7) полученный пароль, при желании, обрезаем (вариант обрезки сохраняем в открытой базе???)
 
-Должна быть возможность добавалять специальные символы, если некоторым сайтам будет недостаточно стандартных символов.
-По умолчанию, они не добавлются
+Должна быть возможность выбора:
+- работы с терминала/консоли
+- работы через интерфейс
+
+Программа должна работать на Linux/Android/Windows(?)
 """
 
 import os
@@ -64,7 +58,7 @@ def createLUKstring(lenString):
 		string += " "
 	return string
 
-def createLUK():
+def createLUK(lenLUK):
 	with open("LUK",'w') as f:  
 		f.write(createLUKstring(lenLUK))
 
@@ -72,11 +66,10 @@ def getHashString(string):
 	hashObject = hashlib.sha256(bytes(string, 'utf-8'))
 	return hashObject.hexdigest()
 
-def getHashLUK():
-	# При отсутствии LUK, создаём
-	if not os.path.exists("LUK"):
-		createLUK()
+def isLUKfile():
+	return os.path.exists("LUK")
 
+def getHashLUK(lenLUK):
 	# Получаем хэш строки из файла
 	with open("LUK",'r') as f:  
 		return getHashString(f.read())
@@ -86,37 +79,8 @@ def convertToString(listUnicode):
 	step = 8
 	for elementList in listUnicode:
 		# при операции ксор, могут появлятся символы за пределами 33 и 127. Чтобы это избежать, делаем два цикла с автокоррекцией числа
-		while elementList < 33:
-			elementList += step
-		while elementList > 127:
-			elementList -= step
+		while elementList < 33:	elementList += step
+		while elementList > 127: elementList -= step
+
 		string += chr(elementList)
 	return string
-
-
-# Чем больше указано количество символов, тем больше энтропия пароля(наверное)
-lenLUK = 5876
-s = secrets.SystemRandom()
-for it in range(0, 1000000):
-
-	privateKey = "AliceAlice"
-	poluPublicKey = str(s.randint(0, 1000000))
-
-	encryptedPrivateKey = getHashString(privateKey)
-	encryptedPoluPublicKey = getHashString(poluPublicKey)
-
-	# Инверсируем элемнеты, так как при операции XOR, одинаковые числа не должны превращатся ноль.
-	UnicodePrivateKey = convertToUnicode(encryptedPrivateKey)[::-1]
-	UnicodePoluPublicKey = convertToUnicode(encryptedPoluPublicKey)
-
-	A = encryptionXOR(UnicodePoluPublicKey, UnicodePrivateKey)
-
-	hashLUK = getHashLUK()
-	B = convertToUnicode(hashLUK)
-
-	result = convertToString(encryptionXOR(A, B))
-
-	a = result.find(" ")
-	if a != -1:
-		print(f"{it} {poluPublicKey} {result}")
-		# break
