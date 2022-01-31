d8389c13edf6f4417c3ed7002818c63ce49ac48b
Anakin <44977098+AnD13@users.noreply.github.com>
2022-01-31 18:00:02 +0300

    the console version is implemented

--- Main.py
+++ Main.py
@@ -1,12 +1,49 @@
 import GenPass as gp
 
-# По умолчанию, используем вариант с интерфейсом. Если False, то работает в консольном режиме
+def processingNumLUK(number):
+	# Если нажать Enter, то устанавливаются значения по умолчанию равным 5000.
+	if number == "":
+		number = 5000
+		return int(number)
+
+	try:
+		if int(number) < 1001:
+			print(list_messages[2])
+			print(list_messages[1])
+			# Сигнал о том, что вводимые данные не прошли условия
+			return -1
+		elif int(number) > 1000:
+			return int(number)
+	except:
+		print(list_messages[3])
+		print(list_messages[1])
+		# Сигнал о том, что вводимые данные не прошли условия
+		return -1
+
+
+def getNumLUKsymbols():
+	while True:
+		# Чем больше указано количество символов, тем больше энтропия LUK ключа(наверное)
+		numberLUKsymbols = input(list_messages[0])
+		
+		numberLUKsymbols = processingNumLUK(numberLUKsymbols)
+		if numberLUKsymbols != -1:
+			return numberLUKsymbols
+
+
+
+list_messages = ["Введите количество символов в LUK файле. Число должно быть больше 1000. По умолчанию задано число 5000: ",
+				"\n=================================================================",
+				"*** Error. Слишком маленькое число. Число должно быть больше 1000",
+				"*** Error. Не верный ввод. Используйте только числа."]
+
+# По умолчанию True - использует вариант с интерфейсом. Если False, то работает в консольном режиме
 UI = False
+
 
 if UI:
 	pass
 else:
-	
 	privateKey = input("Введите приватный ключ: ")
 	referenceWord = input("Введите слово-ориентир: ")
 
@@ -14,36 +51,20 @@ else:
 	encryptedPoluPublicKey = gp.getHashString(referenceWord)
 
 	# Инверсируем элемнеты, так как при операции XOR, одинаковые числа не должны превращатся ноль.
-	UnicodePrivateKey = gp.convertToUnicode(encryptedPrivateKey)[::-1]
+	UnicodePrivateKey = gp.convertToUnicode(encryptedPrivateKey)#[::-1]
 	UnicodePoluPublicKey = gp.convertToUnicode(encryptedPoluPublicKey)
 
 	A = gp.encryptionXOR(UnicodePoluPublicKey, UnicodePrivateKey)
 
 	# При отсутствии файла LUK, генерируем количество символов и создаём его
 	if not gp.isLUKfile():
-		while True:
-			# Чем больше указано количество символов, тем больше энтропия LUK ключа(наверное)
-			numberLUKsymbols = input("Введите количество символов в LUK файле. Число должно быть больше 1000. По умолчанию задано число 5000: ")
-			
-			if numberLUKsymbols == "":
-				numberLUKsymbols = 5000
-				print(numberLUKsymbols)
-				break
-			try:
-				if int(numberLUKsymbols) < 1001:
-					print("*** Error. Слишком маленькое число. Число должно быть больше 1000")
-					print("=================================================================")
-				elif int(numberLUKsymbols) > 1000:
-					break
-			except:
-				print("*** Error. Не верный ввод. Используйте только числа.")
-				print("=================================================================")
-
-		
-	# 	gp.createLUK(numberLUKsymbols)
+		numberLUKsymbols = getNumLUKsymbols()
+		gp.createLUK(numberLUKsymbols)
 	
-	# hashLUK = gp.getHashLUK(numberLUKsymbols)
-	# B = gp.convertToUnicode(hashLUK)
+	# Если не изменять Хэш на все заглавные буквы, то конечный пароль будет без заглавных букв (из-за особенностей XOR). Что уменьшает сложность самого пароля. 
+	hashLUK = gp.getHashLUK().upper()
 
-	# result = gp.convertToString(gp.encryptionXOR(A, B))
-	# print(result)
+	B = gp.convertToUnicode(hashLUK)
+
+	result = gp.convertToString(gp.encryptionXOR(A, B))
+	print(result)
