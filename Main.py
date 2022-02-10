9a3e3916670cbec9cf00777fa37a0cc49673993d
Anakin <44977098+AnD13@users.noreply.github.com>
2022-02-10 23:24:55 +0300

    comment: adding English to comments

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import UI
 import sysconfig
 
 def processingNumLUK(number):
+	# If you press Enter, the default values are set to 5000.
 	# Если нажать Enter, то устанавливаются значения по умолчанию равным 5000.
 	if number == "":
 		number = 5000
@@ -13,21 +14,28 @@ def processingNumLUK(number):
 		if int(number) < 1001:
 			print(list_messages[2])
 			print(list_messages[1])
+			# Input Error. Values should be less than 1000 
+			# Ошибка ввода. Значения должны быть меньше 1000
+			# The signal that the data entered did not pass conditions 
 			# Сигнал о том, что вводимые данные не прошли условия
 			return -1
 		elif int(number) > 1000:
 			return int(number)
 	except:
+		# Input Error. Symbols should not be used 
+		# Ошибка ввода. Не должны использоваться символы
 		print(list_messages[3])
 		print(list_messages[1])
+		# The signal that the data entered did not pass conditions 
 		# Сигнал о том, что вводимые данные не прошли условия
 		return -1
 
 
 def getNumLUKsymbols():
 	while True:
-		# Чем больше указано количество символов, тем больше энтропия LUK ключа(наверное)
 		numberLUKsymbols = input(list_messages[0])
+		# The greater the number of characters specified, the greater the entropy of the LUK-file
+		# Чем больше указано количество символов, тем больше энтропия ЛУК-файла
 		
 		numberLUKsymbols = processingNumLUK(numberLUKsymbols)
 		if numberLUKsymbols != -1:
@@ -54,10 +62,13 @@ if sysconfig.get_platform() == "linux-x86_64":
 elif sysconfig.get_platform() == "linux-aarch64": 
 	CURRENT_OS = TYPE_OS[1]
 
+			# [:-1] Cut a new line symbol. He's not needed 
+			# [:-1] Отрезаем символ новой строки. Он не нужен
 
-# По умолчанию True - используется вариант с интерфейсом. Если False, то работает в консольном режиме
 useUserInterface = False
 
+# If True is used by the interface option. If False, it works in console mode
+# Если True - используется вариант с интерфейсом. Если False, то работает в консольном режиме
 
 
 if useUserInterface:
@@ -70,18 +81,21 @@ else:
 	encryptedPrivateKey = gp.getHashString(privateKey)
 	encryptedPoluPublicKey = gp.getHashString(referenceWord)
 
-	# Инверсируем элемнеты, так как при операции XOR, одинаковые числа не должны превращатся ноль.
+	# Invert the elements, since during the XOR operation, the same keys should not turn zero. And when the keys were swapped, the same password was not created.
+	# Инверсируем элемнеты, так как при операции XOR, одинаковые ключи не должны превращатся ноль. И при перестановке ключей местами, не создавался одинаковый пароль.
 	UnicodePrivateKey = gp.convertToUnicode(encryptedPrivateKey)[::-1]
 	UnicodePoluPublicKey = gp.convertToUnicode(encryptedPoluPublicKey)
 
 	A = gp.encryptionXOR(UnicodePoluPublicKey, UnicodePrivateKey)
 
-	# При отсутствии файла LUK, генерируем количество символов и создаём его
+	# In the absence of a LUK-file, we generate the number of characters and create it
+	# При отсутствии ЛУК-файла, генерируем количество символов и создаём его
 	if not gp.isLUKfile():
 		numberLUKsymbols = getNumLUKsymbols()
 		gp.createLUK(numberLUKsymbols)
 	
-	# Если не изменять Хэш на все заглавные буквы, то конечный пароль будет без заглавных букв (из-за особенностей XOR). Что уменьшает сложность самого пароля. 
+	# If you do not change the hash to all capital letters, then the final password will be without capital letters (why?). Which reduces the complexity of the password itself.
+	# Если не изменять Хэш на все заглавные буквы, то конечный пароль будет без заглавных букв (почему?). Что уменьшает сложность самого пароля. 
 	hashLUK = gp.getHashLUK().upper()
 
 	B = gp.convertToUnicode(hashLUK)
