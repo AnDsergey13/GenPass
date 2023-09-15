8dfe6db3037e3b4f8e514ce85c7e73d162315cb8
Anakin <None@gmail.com>
2023-09-15 21:06:23 +0300

    Lots of changes 1) Added key check in Config.py. Using a decorator 2) Commands for operating systems have been changed. And the description of some commands has been changed 3) Extended method for creating Config.py 4) Other minor changes

--- Config.py
+++ Config.py
@@ -4,13 +4,22 @@ import subprocess
 
 import System
 
+
+def in_key(func):
+	def wrapper(key, *args):
+		""" Проверяем, содержится ли введённый ключ в списке ключей"""
+		if key in ConfigManager.getListAvailableKeys():
+			return func(key, *args)
+		else:
+			print(f"Параметр KEY, указан не верно! Допустимые значения = {ConfigManager.getListAvailableKeys()}")
+	return wrapper
+
 # Декоратор для запуска необходимой команды из списка команд
-# Как работает wrapper и decorator???
 def run_command(commandName):
 	def decorator(func):
 		def wrapper():
 			CurrentOS = System.getCurrentOS()
-			allCommands = OS_Conmands.get(CurrentOS)
+			allCommands = ConfigManager.OS_Conmands.get(CurrentOS)
 			command = allCommands.get(commandName)
 			# Возвращаем результат лямбда-функции из списка команд
 			return command()
@@ -21,35 +30,41 @@ def run_command(commandName):
 class ConfigManager:
 	nameFile = "Config.json"
 
+	listAvailableKeys = ["Database", "LANG", "LUK", "GUI"]
+
 	OS_Conmands = {
 		"Linux": {
-			"isFile" : lambda: os.path.exists(__class__.nameFile),
-			"getStandartPath": lambda: subprocess.check_output("pwd").decode("utf-8").strip() + "/",
-			"newCommand" : None 
-		},
-		"Windows": {
 			"isFile": lambda: os.path.exists(__class__.nameFile),
 			"getStandartPath": lambda: subprocess.check_output("pwd").decode("utf-8").strip() + "/",
-			"newCommand" : None 
+			# Пример: полученную строку с названиями файлов "en_US\nes_ES.json\nru_RU" преобразуем в ["en_US", "es_ES.json", "ru_RU"]
+			"getFileNames": lambda: subprocess.check_output(["ls", "locale"]).decode("utf-8").split("\n")[:-1]
+		},
+		"Windows": {
+			# Проверить команды для Windows
+			"isFile": None,
+			"getStandartPath": None,
+			"getFileNames": None
 		},
 		"Android": {
-			"isFile": None,  # Добавить команду для Android, если необходимо
-			"getStandartPath": None,  # Добавить команду для Android, если необходимо
-			"newCommand" : None 
+			# Проверить команды для Android
+			"isFile": None,
+			"getStandartPath": None,
+			"getFileNames": None
 		}
 	}
 
 	# Нужен ли этот метод???
-	# Получаем название файла настроек
 	@staticmethod
 	def getNameFile():
+		# Получаем название файла настроек
 		return __class__.nameFile
 
 	# Нужен ли этот метод???
-	# Изменяем название файла настроек
 	@staticmethod
 	def setNameFile(newName):
+		# Изменяем название файла настроек
 		__class__.nameFile = newName
+
 
 	@run_command("isFile")
 	def __isFile():
@@ -59,54 +74,99 @@ class ConfigManager:
 	def __getStandartPath():
 		pass
 
+	@run_command("getFileNames")
+	def __getFileNames():
+		pass
+	
+
+	@staticmethod
+	def __get_formated_list_file_names(list_file_names):
+		# Удаляем расширение .json в именах файлов, чтобы проще было потом проверять
+		return [nameFile.replace(".json", "") for nameFile in list_file_names]
+	
+	@staticmethod
+	def check_language_in_list(language, list_file_names):
+		return language in list_file_names
+
 
 	def __getDataInFile(nameFile="Config.json"):
+		""" Получаем все данные из конфигурационного файла"""
 		with open(nameFile) as f:
 			return json.load(f)
 
 	def __setDataInFile(data, nameFile="Config.json"):
+		""" Перезаписываем данные из конфигурационного файла"""
 		with open(nameFile, "w") as f:
 			json.dump(data, f)
-
+	
+	# todo Сделать проверку на правильность введённых путей(pathLUK, pathDatabase)
+	# todo Сделать проверку на правильность введённого (use_GUI)
 	@staticmethod
-	def create(pathLUK="", pathDatabase=""):
-		""" Пример вызова.
-			create("/", "/home")
+	def createFile(use_GUI=True, LANG="", pathLUK="", pathDatabase=""):
+		""" Создаёт конфигурационный файл с базовыми настройками
+			Пример вызова.
+			createFile(True, "es_ES", "/", "/home")
+			ConfigManager.createFile(LANG="en_EN")
 		"""
-		# Существует ли такой файл?
+		# Существует ли такой файл? Проверка сделана с целью защиты от перезаписи файла при повторном запуске функции
 		if not __class__.__isFile():
 
-			# Если путь в атрибутах не задан, то выбирается текущий каталог
+			# Устанавливаем значния по умолчанию, если язык не был введён
+			if LANG == "":
+				LANG = System.getLanguageOS()
+			# Проверяем введённый язык на правильность ввода
+			elif not __class__.check_language_in_list(
+				LANG, 
+				__class__.__get_formated_list_file_names(__class__.__getFileNames())
+				):
+				# И если он не правильный, то автоматически выбираем язык по умолчанию
+				LANG = System.getLanguageOS()
+				print(f"Указанного языка не существует. Выбран {LANG} по умолчанию")
+			
+			# Устанавливаем значния по умолчанию, если путь не был задан
 			if pathLUK == "":
 				pathLUK = __class__.__getStandartPath()
 			if pathDatabase == "":
 				pathDatabase = __class__.__getStandartPath()
 
-			# Cоздать файл хранения путей. Для LUK-файла и для файла базы данных
+			# Cоздать файл хранения настроек
 			data = {}
-			data["LUK"] = pathLUK
-			data["Database.json"] = pathDatabase
+			data["use_GUI"] = str(use_GUI)
+			data["LANG"] = LANG
+			data["pathLUK"] = pathLUK
+			data["pathDatabase"] = pathDatabase
 
 			__class__.__setDataInFile(data)
 
 	@staticmethod
-	def getPath(key):
+	def getListAvailableKeys():
+		""" Получаем список доступных ключей для файла конфигураций"""
+		return __class__.listAvailableKeys
+
+	@staticmethod
+	@in_key
+	def getValue(key):
 		""" Пример вызова.
-			getPath("Database.json")
+			getValue("pathDatabase")
 			Вывод:
 			/home/user/GenPass/Database.json
 		"""
-		# То есть. Выводим путь каталога /home/user/GenPass, а потом в конец добавляем название самого файла
+		# Получаем данные
 		data = __class__.__getDataInFile()
-		return data[key] + key
+		return data[key]
 
 	@staticmethod
-	def changePath(key, newPath):
+	@in_key
+	def changeValue(key, newValue):
 		""" Пример вызова.
-			changePath("LUK", "/etc/luk_files")
+			ConfigManager.changeValue("pathLUK", "/home/Work/Python/")
+			ConfigManager.changeValue("LANG", "en_US")
 		"""
-		# Получаем данные 
+		# Получаем данные
 		data = __class__.__getDataInFile()
-		data[key] = newPath
+		# Изменяем на новые
+		data[key] = newValue
+		# Записываем в файл конфигурации
 		__class__.__setDataInFile(data)
 
+# ConfigManager.createFile()
