de463e146590c5196965561ec16e0b39b6239908
Anakin <None@gmail.com>
2023-10-05 18:15:54 +0300

    beeware(flake8): uncomment (turn off) 'ignore too many blank lines' check style(beeware_gui): format and comment a little refactor(beeware_gui): rename 'System' and 'Config' into 'system' and 'config'

--- templates.py
+++ templates.py
@@ -1,5 +1,5 @@
-import System
-from Config import get_value_by_key
+import system
+from config import get_value_by_key
 
 
 class TemplatesManager:
@@ -50,20 +50,20 @@ class TemplatesManager:
 		self.__full_default_path = self.__default_path + "/" + self.__name_file
 
 		# Создаём пустой файл шаблонов, если его нет
-		if not System.is_file(self.__name_file, self.__default_path):
+		if not system.is_file(self.__name_file, self.__default_path):
 			# TODO переименовать переменные
 			empty_data = {}
 			data = self.__add_empty_template(empty_data)
-			System.set_data_to_file(data=data, json_file_name=self.__full_default_path)
+			system.set_data_to_file(data=data, json_file_name=self.__full_default_path)
 
-		self.__all_templates = System.get_data_from_file(self.__full_default_path)
+		self.__all_templates = system.get_data_from_file(self.__full_default_path)
 		# Все изменения будем производить с копией данных файла шаблонов
 		# Нужно для того, чтобы работала отмена изменений в файле(при запуске GUI)
 		self.__modified_all_templates = self.__all_templates.copy()
 
 		self.__default_mode = False
 
-	
+
 	def __add_empty_template(self, templates):
 		"""Добавляет пустой/базовый шаблон в список шаблонов"""
 		templates[""] = __class__.__default_template
@@ -72,7 +72,7 @@ class TemplatesManager:
 
 	def save_modified_templates(self):
 		"""Сохранить изменённные шаблоны в файл шаблонов"""
-		System.set_data_to_file(data=self.__modified_all_templates, json_file_name=self.__full_default_path)
+		system.set_data_to_file(data=self.__modified_all_templates, json_file_name=self.__full_default_path)
 
 	def cancel_modified_templates(self):
 		""" """
@@ -81,7 +81,7 @@ class TemplatesManager:
 
 	def set_mode_working_with_template(self, new_mode: bool):
 		"""Задать режим работы с шаблоном
-		
+
 		При new_mode=False создаётся новый шаблон, при mode=True выбранный шаблон изменяется"""
 		self.__default_mode = new_mode
 
@@ -134,7 +134,7 @@ print(tp.get_names_all_templates())
 
 # def create_empty_template(self, template_name, **kwargs):
 # 	""" """
-	
+
 # 	if kwargs == None:
 # 		pass
 # 	pass
