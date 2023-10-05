5c1433d63cc924b9131eaf52ef659e5a113add9b
Anakin <None@gmail.com>
2023-10-05 23:55:41 +0300

    beeware_gui:

--- beeware_gui.py
+++ beeware_gui.py
@@ -1,22 +1,40 @@
-# TODO
-# + Добавить простейший пример
-# + Провести сборку под Android
-
 import toga
+from colosseum import CSS
+from colosseum.dimensions import Size, Box
+from localization import get_text as _
 
 class UIBW:
 	""" """
 
-	def __init__(self, name_app):
-		self.name_app = "GenPass"
-
+	def __init__(self, name_app="GenPass"):
+		self.name_app = name_app
+		self.general_padding = 50
+		
 	def __build(self, app):
+		box_lable_1 = toga.Box()
+		label_private_key = toga.Label(text=_("Enter. Private key"))
+		box_lable_1.add(label_private_key)
+
 		box = toga.Box()
 
-		button = toga.Button("Hello world", on_press=self.button_handler)
-		button.style.padding = 50
-		button.style.flex = 1
-		box.add(button)
+		label_private_key.style.padding = self.general_padding
+		label_private_key.style.flex = 2
+		box.add(label_private_key)
+
+		private_key = toga.PasswordInput()
+		private_key.style.padding = self.general_padding
+		private_key.style.flex = 2
+		box.add(private_key)
+
+		is_visible_private_key = toga.Switch(text=_("Visible"))
+		is_visible_private_key.style.padding = self.general_padding
+		private_key.style.flex = 1
+		box.add(is_visible_private_key)
+
+		# button = toga.Button("Hello world", on_press=self.button_handler)
+		# button.style.padding = self.general_padding
+		# button.style.flex = 1
+		# box.add(button)
 
 		return box
 
@@ -28,4 +46,6 @@ class UIBW:
 		print("hello")
 
 
+
 a = UIBW()
+a.create_window()
