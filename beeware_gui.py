905d0fc1e586f0df5ede4ef51486688b8c905a34
Anakin <None@gmail.com>
2023-10-06 14:55:21 +0300

    Development on the beeware library has stopped Due to the lack of normal documentation. And also, with the lack of necessary functionality

--- beeware_gui.py
+++ beeware_gui.py
@@ -9,42 +9,43 @@ class UIBW:
 
 	def __init__(self, name_app="GenPass"):
 		self.name_app = name_app
-		self.general_padding = 50
-		
+		self.general_padding = 20
+		self.private_key_visible = True
+
 	def __build(self, app):
-		box_lable_1 = toga.Box()
-		label_private_key = toga.Label(text=_("Enter. Private key"))
-		box_lable_1.add(label_private_key)
+		self.box = toga.Box()
 
-		box = toga.Box()
+		# label_private_key = toga.Label(text=_("Enter. Private key"))
+		# label_private_key.style.padding = self.general_padding
+		# label_private_key.style.flex = 2
+		# box.add(label_private_key)
 
-		label_private_key.style.padding = self.general_padding
-		label_private_key.style.flex = 2
-		box.add(label_private_key)
+		self.private_key = toga.PasswordInput(placeholder=_("Enter. Private key"))
+		self.private_key.style.padding = self.general_padding
+		self.private_key.style.flex = 2
+		self.box.add(self.private_key)
 
-		private_key = toga.PasswordInput()
-		private_key.style.padding = self.general_padding
-		private_key.style.flex = 2
-		box.add(private_key)
-
-		is_visible_private_key = toga.Switch(text=_("Visible"))
+		is_visible_private_key = toga.Switch(text=_("Visible"), on_change=self.run_switch_private_key)
 		is_visible_private_key.style.padding = self.general_padding
-		private_key.style.flex = 1
-		box.add(is_visible_private_key)
+		is_visible_private_key.style.flex = 1
+		self.box.add(is_visible_private_key)
 
 		# button = toga.Button("Hello world", on_press=self.button_handler)
 		# button.style.padding = self.general_padding
 		# button.style.flex = 1
 		# box.add(button)
 
-		return box
+		return self.box
+
+	def run_switch_private_key(self, widget):
+		ic(self.private_key.remove)
+		return self.box
 
 	def create_window(self):
 		toga.App(self.name_app, "org.beeware.helloworld", startup=self.__build).main_loop()
 
 
 	def button_handler(self, widget):
-		print("hello")
 		ic("hello")
 
 
